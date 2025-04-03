babaf9252a2d70743df510ac0b5dbc002c5a88cc
RayyanFurqan <140888668+RayyanFurqan@users.noreply.github.com>
2025-04-03 21:50:54 +0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lstChat = New System.Windows.Forms.ListBox()
+        Me.txtUserInput = New System.Windows.Forms.TextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.lblSuggestions = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lstChat
+        '
+        Me.lstChat.BackColor = System.Drawing.Color.Black
+        Me.lstChat.Font = New System.Drawing.Font("Arial", 10.0!)
+        Me.lstChat.ForeColor = System.Drawing.Color.LightGreen
+        Me.lstChat.FormattingEnabled = True
+        Me.lstChat.ItemHeight = 19
+        Me.lstChat.Location = New System.Drawing.Point(10, 10)
+        Me.lstChat.Name = "lstChat"
+        Me.lstChat.Size = New System.Drawing.Size(460, 289)
+        Me.lstChat.TabIndex = 0
+        '
+        'txtUserInput
+        '
+        Me.txtUserInput.BackColor = System.Drawing.Color.White
+        Me.txtUserInput.Location = New System.Drawing.Point(10, 320)
+        Me.txtUserInput.Name = "txtUserInput"
+        Me.txtUserInput.Size = New System.Drawing.Size(360, 22)
+        Me.txtUserInput.TabIndex = 1
+        '
+        'btnSend
+        '
+        Me.btnSend.BackColor = System.Drawing.Color.DarkBlue
+        Me.btnSend.ForeColor = System.Drawing.Color.White
+        Me.btnSend.Location = New System.Drawing.Point(380, 320)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(90, 30)
+        Me.btnSend.TabIndex = 2
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = False
+        '
+        'lblSuggestions
+        '
+        Me.lblSuggestions.BackColor = System.Drawing.Color.DarkBlue
+        Me.lblSuggestions.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSuggestions.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!)
+        Me.lblSuggestions.ForeColor = System.Drawing.Color.White
+        Me.lblSuggestions.Location = New System.Drawing.Point(513, 65)
+        Me.lblSuggestions.Name = "lblSuggestions"
+        Me.lblSuggestions.Size = New System.Drawing.Size(300, 100)
+        Me.lblSuggestions.TabIndex = 3
+        Me.lblSuggestions.Text = "Label1"
+        Me.lblSuggestions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(860, 450)
+        Me.Controls.Add(Me.lblSuggestions)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.txtUserInput)
+        Me.Controls.Add(Me.lstChat)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstChat As ListBox
+    Friend WithEvents txtUserInput As TextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents lblSuggestions As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+
+Public Class Form1
+    ' Dictionary for chatbot responses
+    Private responses As New Dictionary(Of String, List(Of String)) From {
+        {"hello", New List(Of String) From {"Hi there!", "Hello! How can I help?", "Hey! What's up?"}},
+        {"how are you", New List(Of String) From {"I'm just a bot, but I'm doing great!", "Feeling chatty today!", "All good! What about you?"}},
+        {"bye", New List(Of String) From {"Goodbye!", "See you later!", "Take care!"}},
+        {"what is your name", New List(Of String) From {"I'm ChatBot!", "Call me AI Helper.", "I am your friendly chatbot."}},
+        {"default", New List(Of String) From {"I didn't understand that.", "Can you rephrase?", "I'm still learning, try again!"}}
+    }
+
+    ' Handle Send Button Click
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+        Dim userMessage As String = txtUserInput.Text.ToLower().Trim()
+        If userMessage = "" Then Exit Sub ' Prevent empty messages
+
+        ' Display User Message
+        lstChat.Items.Add("You: " & txtUserInput.Text)
+
+        ' Get AI Response
+        Dim botReply As String = GetResponse(userMessage)
+        lstChat.Items.Add("Bot: " & botReply)
+
+        ' Clear input field
+        txtUserInput.Clear()
+    End Sub
+
+    ' Function to Get AI Response
+    Private Function GetResponse(input As String) As String
+        ' Check for matching keyword
+        For Each key In responses.Keys
+            If input.Contains(key) Then
+                Dim possibleReplies As List(Of String) = responses(key)
+                Dim random As New Random()
+                Return possibleReplies(random.Next(possibleReplies.Count))
+            End If
+        Next
+        ' Default response if no match
+        Dim defaultReplies As List(Of String) = responses("default")
+        Dim rand As New Random()
+        Return defaultReplies(rand.Next(defaultReplies.Count))
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Set the text of lblSuggestions to display example questions
+        lblSuggestions.Text = "Try asking:" & vbCrLf &
+                          "- Hello" & vbCrLf &
+                          "- How are you?" & vbCrLf &
+                          "- What is your name?" & vbCrLf &
+                          "- Tell me a joke" & vbCrLf &
+                          "- What can you do?" & vbCrLf &
+                          "- Who created you?"
+    End Sub
+
+
+End Class
